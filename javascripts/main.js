802c3f61c722ca1b5aa4c041d63cb5c1b7250a7b
Tyler Barnett <tyler.barnett7190@gmail.com>
2017-08-22 21:57:40 -0500

    finally at MVP

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,7 +1,7 @@
 "use strict";
 
 let Handlebars = require('hbsfy/runtime');
-let explosivesDropdownTemplate = require("../templates/dropdownTemp.hbs");
+// let explosivesProductTemplate = require("../templates/productTemplate.hbs");
 let explosivesFactory = require("./explosives-factory.js");
 
 let categoryDropdown = $("#futureDropdown");
@@ -52,35 +52,37 @@ function createTypesDropDown(arrayOfChosenType) {
         typeDropdown.html(outputTypeDropdown);
             $(".dropdown-radio-type").click(function(event) {
                 // console.log ("event", event);
+                arrayOfProducts = [];
                 selectedTypeDropDown = parseInt($(this).attr("value"));
                 console.log (selectedTypeDropDown);
-
-                $("#dropdownMenu2").html($(this).html());
-    });
 
                 explosivesFactory.loadDetails()
                 .then(
                     (productsFromFactory) => {
-                        console.log ("productsFromFactory", productsFromFactory);
-                    // productsFromFactory.forEach((product) => {     
-                    //         // arrayOfProducts.push(product);
-                    //         console.log (key);
-                    //     });
+                        // console.log ("productsFromFactory", productsFromFactory);
+                        productsFromFactory.forEach(function(product, index) {
+                            for (let prop in product) {
+                                // arrayOfProducts = [];
+                                if (product[prop].type === selectedTypeDropDown) {
+                                    arrayOfProducts.push(product[prop]);
+                                }
+                            }
+                        });
+                                sendToTemplate(arrayOfProducts);
                                 
                 },
 
                     (reject) => {
                     console.log ("something went wrong");
                     });
+                
+                $("#dropdownMenu2").html($(this).html());
+    });
+
         
         arrayOfTypes = [];
         outputTypeDropdown = "";
     }
-
-
-// function checkValueOfSelected() {
-//     console.log ("selectedDropDown", selectedDropDown);
-// }
 
 
 
@@ -96,16 +98,52 @@ explosivesFactory.loadCategories()
     });
 
 
-// ).then (
-// 	() => {
-// 		console.log ("details & all are loaded");
-// 		console.log (explosivesFactory.getAllProducts());
-// 	},
-// 	() => {
-// 		console.log ("no details loaded");
-// 	}
-// );
-
-
 // FUNCTION TO TAKE AN ARRAY AND PLUG IT INTO HANDLEBARS then SPIT TO PAGE
+let outputHTML = '';
+
+
+function sendToTemplate(arrayOfProducts) {
+    console.log ("arrayOfProducts", arrayOfProducts);
+    $("#productsHere").html('');
+    outputHTML = '';
+    for (let i = 0; i < arrayOfProducts.length; i++) {
+        outputHTML += `<div class="col-sm-4">
+                        <div class="card">
+                          <div class="card-block">
+                            <h2 class="card-title">${arrayOfProducts[i].name}</h2>
+                            <h4 class="card-text">${arrayOfProducts[i].description}</h4>
+                            <p>Category: ${getCategory(arrayOfProducts[i].id)}</p>
+                            <p>Type of Product: ${getType(arrayOfProducts[i].type)}</p>
+                          </div>
+                        </div>
+                      </div>`;
+    }
+    $("#productsHere").html(outputHTML);
+    
+}
+
+function getCategory(id) {
+    if (id === 0) {
+        return "Recreation";
+    }else if (id === 1) {
+        return "Demolition";
+    }
+}
+
+function getType(type) {
+    if (type === 0) {
+        return "Personal";
+    }else if (type === 1) {
+        return "Self Propellant";
+    }else if (type === 2) {
+        return "Fishing Assistance";
+    }else if (type === 3) {
+        return "Tree Stump Management";
+    }else if (type === 4) {
+        return "Bird Deterrent";
+    }else if (type === 5) {
+        return "Rock Clearer";
+    }else {return "So new, it isn't catalogued yet!";}
+}
+
 
